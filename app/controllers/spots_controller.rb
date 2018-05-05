c0b5e1eb3ba1d36500da7ace57cb3af56ad96859
Nicolas Guitard <ngui@caramail.fr>
2018-05-05 17:41:43 +0200

    Show, List, Form, Image

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,4 +1,19 @@
 class SpotsController < ApplicationController
   def index
+    @spots = Spot.all
+  end
+  def new
+    @spot = Spot.new
+  end
+  def show
+  @spot = Spot.find(params[:id])
+  end
+  def create
+    @spot = Spot.create(spot_params)
+    redirect_to spots_path
+  end
+  private
+  def spot_params
+  params.require(:spot).permit(:title, :date, :image)
   end
 end
