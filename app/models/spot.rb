7da005da06f82e481a764332bdfee2be7406f2b4
Nicolas Guitard <ngui@caramail.fr>
2018-05-06 12:08:46 +0200

    correctif

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,8 +1,9 @@
 class Spot < ActiveRecord::Base
   validates :user_id, presence: true
   validates :image, presence: true
-  has_attached_file :image, styles: { :medium => "640x" }
+  has_attached_file :image, styles: { :medium => "640x" }, :url => "/images/upload/:style/photo.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
   belongs_to :user
   geocoded_by :adress
   after_validation :geocode
