5228bc768b984f7f8427ce18eeb7ab8394c56f72
Nicolas Guitard <ngui@caramail.fr>
2018-05-07 15:59:52 +0200

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,13 +89,14 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.paperclip_defaults = {
-  storage: :s3,
-  s3_credentials: {
-    bucket: ENV.fetch('ufospt'),
-    access_key_id: ENV.fetch('AKIAIYEMONKCX4WNYT4A'),
-    secret_access_key: ENV.fetch('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'),
-    s3_region: ENV.fetch('eu-west-3'),
-  }
-}
+config.paperclip_defaults = {
+    storage: :s3,
+    s3_region: ENV["eu-west-3"],
+    s3_credentials: {
+      bucket: ENV["ufospt"],
+      access_key_id: ENV["AKIAIYEMONKCX4WNYT4A"],
+      secret_access_key: ENV["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"]
+      }
+    }
+
 end
