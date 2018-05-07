d697baa57d04a36820931d0cc8e878d2021a2f27
Nicolas Guitard <ngui@caramail.fr>
2018-05-07 23:34:08 +0200

    test AWS

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,7 +1,7 @@
 class Spot < ActiveRecord::Base
   validates :user_id, presence: true
   validates :image, presence: true
-  has_attached_file :image, styles: { :medium => "640x" }, url: "/public/upload/:id/:filename"
+  has_attached_file :image, styles: { :medium => "640x" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   belongs_to :user
--- config/deploy.rb
+++
@@ -1,1 +0,0 @@
-set :shared_dirs, fetch(:shared_dirs, []).push('log', 'public/system')
